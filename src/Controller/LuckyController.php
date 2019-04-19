5bca5a0849b4d2b068093e4a1ab552067a58c7c0
avophile <douglas.locklin@gmail.com>
2019-04-19 23:50:58 +0300

    adds annotation for route configs and makes Lucky Controller extend AbstractController

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -3,16 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+//use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class LuckyController
+class LuckyController extends AbstractController
 {
-    /**
-     * @Route("/lucky/number/{max}", name="app_lucky_number")
-     */
-    public function number($max)
+    public function number()
     {
-        $number = random_int(0, $max);
+        $number = random_int(0, 100);
 
         return new Response(
             '<html><body>Lucky number: '.$number.'</body></html>'
